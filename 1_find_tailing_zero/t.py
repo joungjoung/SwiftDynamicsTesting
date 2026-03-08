00e72095a80576a8556c5025c253e9a70ed74afc
joungjoung <81035345+joungjoung@users.noreply.github.com>
2026-03-08 20:42:26 +0700

    1_test_optimize1

--- 1_find_tailing_zero/t.py
+++ 1_find_tailing_zero/t.py
@@ -23,5 +23,33 @@ def count_trailing_zeroes(n: int) -> int:
         n //= 10
     return count
 
-for n in range(1, 31):
-    print(f"number: {n}, factorial: {factorial_component(n)}, trailing zeroes: {count_trailing_zeroes(factorial(n))}")
+def components_5n(n: int) -> int:
+    """
+    หาจำนวนครั้งที่ 5 เป็นตัวประกอบในเลข n! โดยการนับจำนวนเลขที่หารด้วย 5 ได้ลงตัว
+    """
+
+    return n // 5
+
+def components_25n(n: int) -> int:
+    """
+    หาจำนวนครั้งที่ 25 เป็นตัวประกอบในเลข n! โดยการนับจำนวนเลขที่หารด้วย 25 ได้ลงตัว
+    """
+
+    return n // 25
+
+def max_num_power_5(n: int) -> int:
+    """
+    หาจำนวนของเลขที่เป็นกำลังของ 5 ที่น้อยที่สุดที่น้อยกว่าหรือเท่ากับ n
+    """
+
+    count = 0
+    power_of_5 = 5
+    while power_of_5 <= n:
+        count += 1
+        power_of_5 *= 5
+    return count
+
+
+for n in range(1, 126):
+    # print(f"number: {n}, components of 5: {components_5n(n)}, components of 25: {components_25n(n)}, trailing zeroes: {count_trailing_zeroes(factorial(n))}")\
+    print(f"number: {n}, max power of 5: {max_num_power_5(n)}, trailing zeroes: {count_trailing_zeroes(factorial(n))}")
